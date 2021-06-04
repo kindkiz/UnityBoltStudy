4a3a259b43db98ab8bbfcc1af319f39f07ca63be
HAN TAE GYUN <kindkiz@naver.com>
2021-06-04 13:38:06 +0900

    feat: Update RnC

---
+++ roll and collect/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int totalItemCount;
+    public int stage;
+    public Text stageCount;
+    public Text playerCount;
+    public Text stageText;
+
+
+    private void Awake()
+    {
+        stageCount.text = "/ " + totalItemCount;
+        stageText.text = "Stage "+ stage;
+    }
+
+    public void getItem(int count)
+    {
+        playerCount.text = count.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(stage);
+        }
+    }
+}
---
+++ roll and collect/Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    public int itemCount;
+    public GameManager manager;
+    bool isJump;
+    AudioSource audio;
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        isJump = false;
+        audio = GetComponent<AudioSource>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(h, 0, v);
+        rigid.AddForce(vec, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "floor")
+        {
+            isJump = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            itemCount++;
+            manager.getItem(itemCount);
+            audio.Play();
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "finish")
+        {
+            if (manager.totalItemCount == itemCount)
+            {
+                // Game Clear!
+                if (manager.stage == 5)
+                    SceneManager.LoadScene("Example1_0");
+                else
+                    SceneManager.LoadScene("Example1_" + (manager.stage + 1).ToString());
+            }
+            else
+            {
+                // Game Restart...
+                SceneManager.LoadScene("Example1_" + manager.stage.ToString());
+            }
+            itemCount++;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
