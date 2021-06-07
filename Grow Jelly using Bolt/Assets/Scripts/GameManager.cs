844be22418ae041a5d5ca60c758c8efcf499e66d
HAN TAE GYUN <kindkiz@naver.com>
2021-06-07 15:40:40 +0900

    feat: Update Jelly Farm

---
+++ Grow Jelly using Bolt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] JellySpriteList;
+    public string[] JellyNameList;
+    public int[] JellyJelatinList;
+    public int[] JellyGoldList;
+    public int[] numGoldList;
+    public int[] clickGoldList;
+
+    public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
+}
