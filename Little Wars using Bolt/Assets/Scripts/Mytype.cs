db811ba44bc5206e6690ed5df0cbdbafd16b882d
HAN TAE GYUN <kindkiz@naver.com>
2021-06-10 00:51:08 +0900

    feat: Upload little wars

---
+++ Little Wars using Bolt/Assets/Scripts/Mytype.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mytype
+{ 
+    public enum Unit { Sword, Range, Guard, Wizard, Bullet };
+    public enum Sound { Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic }
+}
